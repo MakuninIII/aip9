89186f7d4158ca942a380c17d59c6555780dbfbc
MakuninIII <imac.55505@gmail.com>
2023-05-31 14:26:06 +0300

    adasd

---
+++ build/CMakeFiles/Program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program.dir/test.cpp.obj"
+  "CMakeFiles/Program.dir/test.cpp.obj.d"
+  "Program.exe"
+  "Program.exe.manifest"
+  "Program.pdb"
+  "libProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
